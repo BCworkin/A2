5017c6103afce338c28fdae0e1331d356c199dfd
chibj006 <bryanchiamjf01@gmail.com>
2024-05-20 22:17:52 +0930

    updated constructor, and both add methods

--- src/unisa/dse/a2/students/DSEList.java
+++ src/unisa/dse/a2/students/DSEList.java
@@ -1,6 +1,7 @@
 package unisa.dse.a2.students;
 
 import unisa.dse.a2.interfaces.List;
+import unisa.dse.a2.students.Node;
 import java.util.*;
 
 /**
@@ -11,64 +12,59 @@ public class DSEList implements List {
 	
 	public Node head = null;
 	private Node tail =  null;
-	private int size = 0;
+	private int size;
 	
-	public ListIterator<String> listIterator;
-
+	//done
 	public DSEList() {
 	}
 	
+	//done
 	public DSEList(Node head_) {
-		head = head_;
+		this.head = head_;
+		this.tail = head_;
+		this.size = 1;
 	}
 	
 	//Takes a list then adds each element into a new list
+	//done
 	public DSEList(DSEList other) { // Copy constructor. 
-		if (other.head == null) {
-			this.head = null; 
-		} else {
-			Node othernode = other.head;
-			this.head = othernode;
-			this.tail = other.tail;
-			this.size = other.size;
-		}
+	       if (other == null) {
+	            return;
+	        }
+
+	        Node current = other.head;
+	        while (current != null) {
+	            current = current.next;
+	        }
 	}
 
 	//remove the String at the parameter's index
 	public String remove(int index) {
-		//debug
-		return listIterator.remove(index);
 	}
 
 	//returns the index of the String parameter 
 	public int indexOf(String obj) {
-		//make changes
-	    int index = 0;
-	    Node current = head;
-	    while (current != null) {
-	        if (current.data.equals(obj)) {
-	            return index;
-	        }
-	        current = current.next;
-	        index++;
-	    }
-	    return -1; 
+
 	}
 	
 	//returns String at parameter's index
 	public String get(int index) {
 		if(index  < 0 || index >= size) {
 			throw new IndexOutOfBoundsException(Integer.toString(index));
+		} else {
+			return 
 		}
-		return listIterator(index).next();
 		
 	}
 
 	//checks if there is a list
 	public boolean isEmpty() {
+		size = 0;
+		return true;
 	}
 
 	//return the size of the list
+	//done
 	public int size() {
 		return size;
 	}
@@ -80,16 +76,63 @@ public class DSEList implements List {
 
 	//add the parameter String at of the end of the list
 	public boolean add(String obj) {
-		add(size, obj);
-		return true;
 		
+        Node newNode = new Node(null, null, obj);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+        return true;
 	}
 
+
 	//add String at parameter's index
-	public boolean add(int index, String obj) {
-		//make changes
-		listIterator(index).add(obj);
-		return true;
+	public boolean add(int index, String obj) { 
+		
+		if (index < 0 || index > size) {
+	        throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+	    }
+		
+	    Node newNode = new Node(null, null, obj);
+
+	    if (index == 0) { 
+	        newNode.next = head;
+	        if (head != null) {
+	            head.prev = newNode;
+	        }
+	        head = newNode;
+	        if (tail == null) { 
+	            tail = newNode;
+	        }
+	    } else if (index == size) { 
+	        newNode.prev = tail;
+	        if (tail != null) {
+	            tail.next = newNode;
+	        }
+	        tail = newNode;
+	        if (head == null) { 
+	            head = newNode;
+	        }
+	    } else { 
+	        Node temp = head;
+	        for (int i = 0; i < index - 1; i++) {
+	            temp = temp.next;
+	        }
+	        newNode.next = temp.next;
+	        newNode.prev = temp;
+	        if (temp.next != null) {
+	            temp.next.prev = newNode;
+	        }
+	        temp.next = newNode;
+	    }
+
+	    size++;
+	    return true;
+
 	}
 
 	//searches list for parameter's String return true if found
